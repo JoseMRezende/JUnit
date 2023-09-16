e4a00b5ef8a4a96a6f03a93b72effc9dd1511c07
JoseMRezende <rezendinhobr18@gmail.com>
2023-09-16 20:10:34 -0300

    branch JUnit

---
+++ src/JUnitBranch/CalculadoraJunit.java
@@ -0,0 +1,25 @@
+package JUnitBranch;
+
+public class CalculadoraJunit {
+
+        //Método de Soma
+        public int somar(int a, int b) {
+            return a + b;
+        }
+
+        //Método de Subtração
+        public int subtrair(int a, int b){
+            return a - b;
+        }
+
+        //Método de Divição
+        public int divicao(int a, int b){
+            return a / b;
+        }
+
+        //Método de Multiplicação
+        public int multiplicacao(int a, int b){
+            return a * b;
+        }
+
+}
--- src/JUnitBranch/CalculadoraJunitTest.java
+++ src/JUnitBranch/CalculadoraJunitTest.java
@@ -1,4 +1,12 @@
 package JUnitBranch;
 
+import org.junit.jupiter.api.Test;
+
 public class CalculadoraJunitTest {
+
+    @Test
+    public void Somar(){
+        CalculadoraJunit calc = new CalculadoraJunit();
+        int soma = calc.somar(3,7);
+    }
 }
