a76d48122380cca8fea8510cb56510c6695eb891
JoseMRezende <rezendinhobr18@gmail.com>
2023-09-16 23:57:49 -0300

    branch Junit 2.0

--- src/JUnitBranch/CalculadoraJunitTest.java
+++ src/JUnitBranch/CalculadoraJunitTest.java
@@ -5,10 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraJunitTest {
 
+    CalculadoraJunit calc = new CalculadoraJunit();
+
     @Test
     public void Somar(){
-        CalculadoraJunit calc = new CalculadoraJunit();
         int soma = calc.somar(3,7);
         Assertions.assertEquals(10,soma);
     }
+
+    @Test
+    public void Somar2(){
+        int soma2 = calc.somar(10,0);
+        Assertions.assertEquals(10,soma2);
+    }
+
+    @Test
+    public void Somar3(){
+        int soma3 = calc.somar(0,0);
+        Assertions.assertEquals(0,soma3);
+    }
+
+    @Test
+    public void Somar4(){
+        int soma4 = calc.somar(5,-2);
+        Assertions.assertEquals(3,soma4);
+    }
+
 }
